043448b80e99e30f4a4b870801cd720d85521bdb
Loris Cro <kappaloris@gmail.com>
2019-12-31 14:56:10 +0100

    add constructor for CellRef

--- cellref.go
+++ cellref.go
@@ -7,6 +7,10 @@ type CellRef struct {
 	row, col int
 }
 
+func NewCellRef(row int, col int) CellRef {
+	return CellRef{row, col}
+}
+
 func (ref *CellRef) String() string {
 	return fmt.Sprintf("R%vC%v", ref.row+1, ref.col+1)
 }
